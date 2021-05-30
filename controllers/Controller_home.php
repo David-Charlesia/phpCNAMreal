a4cb315eb23e832d062df4cd1d63cdb3746967ba
David-Charlesia <pro.charlesia.david@outlook.com>
2021-05-30 21:15:45 +0200

    w

--- controllers/Controller_home.php
+++ controllers/Controller_home.php
@@ -3,8 +3,6 @@
 class Controller_home extends Controller{
 
   public function action_home(){
-    // $m=Model::getModel();
-    // $tab=['nb_nobels'=>$m->getNbNobelPrizes()];
     $this->render('home');
   }
 
